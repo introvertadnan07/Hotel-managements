76ba5fdd2abe4a366b522bab7eb65262746df677
Md Adnan qaisar <staryounger012@gmail.com>
2026-01-25 11:11:02 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -15,7 +16,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
-    </div>
+      <Footer />    
+      </div>
   );
 };
 
