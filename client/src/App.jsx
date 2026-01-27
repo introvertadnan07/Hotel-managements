e4963523e905bdf01d0cd7f62bc01b523e6449b9
Md Adnan qaisar <staryounger012@gmail.com>
2026-01-27 22:58:45 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./components/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
-  const isOwnerPath = useLocation().pathname.includes("owner");
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("owner");
 
   return (
-    <div>
+    <>
       {!isOwnerPath && <Navbar />}
 
       <div className="min-h-[70vh]">
@@ -18,11 +20,12 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
 
       <Footer />
-    </div>
+    </>
   );
 };
 
