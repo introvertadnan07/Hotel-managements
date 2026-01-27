62695ab23a854985f57222e1ac527adbe3b7554d
Md Adnan qaisar <staryounger012@gmail.com>
2026-01-27 22:58:59 +0530

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
-import { ClerkProvider } from '@clerk/clerk-react'
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error("Missing Clerk Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
