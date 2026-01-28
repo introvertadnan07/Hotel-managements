56e0026602ace6d63d718c9db0e09768532afbc7
Md Adnan qaisar <staryounger012@gmail.com>
2026-01-29 03:14:57 +0530

    Data

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -162,12 +162,12 @@ export const roomCommonData = [
 // User Dummy Data
 export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
-    "username": "Great Stack",
-    "email": "user.greatstack@gmail.com",
-    "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "username": "Anumifly ",
+    "email": "user.anumifly@gmail.com",
+    image: AnumImage,
     "role": "hotelOwner",
-    "createdAt": "2025-03-25T09:29:16.367Z",
-    "updatedAt": "2025-04-10T06:34:48.719Z",
+    "createdAt": "2025-12-25T09:29:16.367Z",
+    "updatedAt": "2026-26-10T06:34:48.719Z",
     "__v": 1,
     "recentSearchedCities": [
         "New York"
@@ -299,6 +299,8 @@ export const dashboardDummyData = {
     "totalRevenue": 897,
     "bookings": userBookingsDummyData
 }
+
+
 
 // --------- SVG code for Book Icon------
 /* 
