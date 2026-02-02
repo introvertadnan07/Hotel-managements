01539ed401a41668514ca12768d1e049066f310c
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-02 22:42:08 +0530

    server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+connectDB()
+
+const app = express()
+app.use(cors()) // Enbale cross-origin Resource Sharing
+
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res)=>res.send("API is working fine Anumifly"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
