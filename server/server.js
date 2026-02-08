059982756ef03614b5aae0dbaf61a280c9e67138
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-08 19:54:55 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
+
+import connectDB from "./configs/db.js";
+import connectCloudinary from "./configs/cloudinary.js";
+
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
-import connectCloudniary from "./configs/cloudinary.js";
 import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB();
-connectCloudniary();
+connectCloudinary();
 
 const app = express();
 
@@ -19,14 +21,17 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
+app.get("/", (req, res) => {
+  res.send("API is working fine Anumifly");
+});
+
 app.use("/api/clerk", clerkWebhooks);
-
-app.get("/", (req, res) => res.send("API is working fine Anumifly"));
-
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
 app.use("/api/bookings", bookingRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
