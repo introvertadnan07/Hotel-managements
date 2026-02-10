0a941d18b04702f29c56e6b9ae993dc53e004194
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-10 10:49:33 +0530

    main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
+import { AppProvider } from "./context/AppContext";
+
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,8 +16,9 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </ClerkProvider>
 );
-
