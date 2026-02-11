eddce36ec6550829c78f6db166d0e0a7da3d5cfd
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-11 11:23:17 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 🔥 REQUIRED
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
