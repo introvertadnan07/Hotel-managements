63de3525378eba0a6c56915e4f2504c674c093c4
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-13 01:17:45 +0530

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context/AppContext";
-
+import { Toaster } from "react-hot-toast";   // ✅ ADD THIS
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -17,6 +17,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
       <AppProvider>
+
+        {/* ✅ THIS IS REQUIRED */}
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 2000,
+          }}
+        />
+
         <App />
       </AppProvider>
     </BrowserRouter>
