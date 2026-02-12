837944fc5a8a25878b9010459c3901a0b1d90ee8
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-13 00:44:04 +0530

    roomRoutes.js

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,9 +1,34 @@
 import express from "express";
-import { createRoom, getRooms } from "../controllers/roomController.js";
+import upload from "../middleware/uploadMiddleware.js";
+import {
+  createRoom,
+  getRooms,
+  getOwnerRooms,
+  toggleRoomAvailability,   // 👈 ADD THIS
+} from "../controllers/roomController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const roomRouter = express.Router();
 
-roomRouter.post("/", createRoom);
+// create room (owner)
+roomRouter.post(
+  "/",
+  protect,
+  upload.array("images", 4),
+  createRoom
+);
+
+// get all rooms (public)
 roomRouter.get("/", getRooms);
+
+// get rooms of logged-in hotel owner
+roomRouter.get("/owner", protect, getOwnerRooms);
+
+// ✅ TOGGLE AVAILABILITY
+roomRouter.post(
+  "/toggle-availability",
+  protect,
+  toggleRoomAvailability
+);
 
 export default roomRouter;
