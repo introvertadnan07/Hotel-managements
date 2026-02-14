c38e7b8358f911806639258585248d60b4e1fa92
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-14 12:24:10 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,12 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 import OwnerLayout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
 import AddRoom from "./pages/hotelOwner/AddRoom";
 import ListRoom from "./pages/hotelOwner/ListRoom";
+
+import Rooms from "./pages/Rooms";
+import RoomDetails from "./components/RoomDetails"; // ✅ IMPORTANT
 
 const App = () => {
   return (
@@ -14,7 +18,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
 
+        {/* Rooms */}
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} /> {/* ✅ FIX */}
+
+        {/* Owner */}
         <Route path="/owner" element={<OwnerLayout />}>
+          <Route index element={<Dashboard />} />
           <Route path="add-room" element={<AddRoom />} />
           <Route path="list-room" element={<ListRoom />} />
         </Route>
