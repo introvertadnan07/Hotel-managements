e36b6e447813c2d08def135e1229eb68d7336bb8
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-14 12:24:27 +0530

    featured jsx

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import HotelCard from "./HotelCard";
-import { roomsDummyData } from "../assets/assets";
 import Title from "./Title";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const FeaturedDestination = () => {
-  
-  const navigate = useNavigate()
+  const { rooms, navigate } = useAppContext();
 
+  if (!rooms || rooms.length === 0) return null;
 
   return (
     <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
@@ -17,13 +16,19 @@ const FeaturedDestination = () => {
       />
 
       <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-        {roomsDummyData.slice(0, 4).map((room, index) => (
+        {rooms.slice(0, 4).map((room, index) => (
           <HotelCard key={room._id} room={room} index={index} />
         ))}
       </div>
-      <button onClick={() =>{navigate('/rooms'); scrollTo(0,0)}}
-      className="mx-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white havor:bg-gray-50 transition-all cursor-pointer">
-        view All Destinations
+
+      <button
+        onClick={() => {
+          navigate("/rooms");
+          window.scrollTo(0, 0);
+        }}
+        className="mt-10 px-5 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer"
+      >
+        View All Destinations
       </button>
     </div>
   );
