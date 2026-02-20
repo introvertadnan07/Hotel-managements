fdf36c1848a5647234ebe4ef71b83fcf8c04bac9
Md Adnan qaisar <staryounger012@gmail.com>
2026-02-20 16:25:56 +0530

    fix: update root endpoint response message for clarity

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
-  res.send("API running...");
+  res.send("Anumifly code work...");
 });
 
 app.use("/api/user", userRouter);
